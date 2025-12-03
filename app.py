3dfacfcf8fcc4c0e591bab14b440d8300a24fa99
takafumi-1124 <108028933+takafumi-1124@users.noreply.github.com>
2025-12-03 14:23:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -189,6 +189,7 @@ with tabs[2]:
             "優先度（%）": (priorities_main * 100).round(1)
         }),
         use_container_width=True
+        hide_index=True
     )
 
     st.write(f"整合性比率 (CR): {cr_main:.3f}")
